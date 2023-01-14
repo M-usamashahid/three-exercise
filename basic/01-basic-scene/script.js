a86da2068558818a699a06d2572f79c30455cee0
M Usama Shahid <usamashahid94@gmail.com>
2023-01-15 01:16:16 +0500

    01 - Scene Creation 02 - Webpack starter

---
+++ basic/01-basic-scene/script.js
@@ -0,0 +1,36 @@
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene();
+
+// Object
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
+
+
+// Sizes
+const sizes = {
+    width: 800,
+    height: 600
+}
+
+// Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+camera.position.z = 3
+// camera.position.y = 0
+// camera.position.x = -3
+scene.add(camera)
+
+
+// Renderer
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.render(scene, camera)
